12939facd630d7419afe11641ab2b6d293a4a2e7
Caroline <carolinekimatu6@gmail.com>
2022-09-09 08:41:49 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints all the numbers of base 16 in lowercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char b;
+	char d = '0';
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+	for (b = 'a'; b <= 'f'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+return (0);
+}
