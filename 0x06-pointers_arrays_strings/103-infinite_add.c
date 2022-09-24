5266b25e223c77744361142e283a8e23f76f5cad
Caroline <carolinekimatu6@gmail.com>
2022-09-24 03:37:42 -0700

    add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -22,6 +22,13 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 		tens = num / 10;
 	}
 
+	for (; *n1; n1--, r_index--)
+	{
+		num = (*n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
 	for (; *n2; n2--, r_index--)
 	{
 		num = (*n2 - '0') + tens;
@@ -66,4 +73,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	n1 += n1_len - 1;
 	n2 += n2_len - 1;
 	*(r + size_r) = '\0';
+
+	return (add_strings(n1, n2, r, --size_r));
 }
