1a6747afa5328e7763096c4a0f0083e87a0b7409
Caroline <carolinekimatu6@gmail.com>
2022-10-11 07:04:37 -0700

    initial commit

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Projects about structures and typedef`
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new type desribing a dog
+ * @name:first member of the structure
+ * @age: second member
+ * @owner: third member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /*DOG_H*/
