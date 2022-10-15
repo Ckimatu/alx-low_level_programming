4069e23f44cc0f45b3b86b3dd605a9072742e916
Caroline <carolinekimatu6@gmail.com>
2022-10-14 22:44:08 -0700

    correction of header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,12 +2,9 @@
 #define FUNCTION_POINTERS_H
 #include <stdlib.h>
 
-/**
- * Description: header file containing prototypes of all functions to be used
- */
-
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));int int_index(int *array, int size, int (*cmp)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char c);
 
 #endif /*FUNCTION_POINTERS_H*/
